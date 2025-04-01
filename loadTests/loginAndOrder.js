b01229c15b1e48809b567e6dc7f3a0ba21f32f52
tylerhiatt <tyler.hiatt@byu.edu>
2025-04-01 16:03:10 -0600

    Added grafana load test loginAndOrder

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,298 @@
+import { sleep, check, group, fail } from "k6";
+import http from "k6/http";
+import jsonpath from "https://jslib.k6.io/jsonpath/1.0.2/index.js";
+
+export const options = {
+  cloud: {
+    distribution: {
+      "amazon:us:ashburn": { loadZone: "amazon:us:ashburn", percent: 100 },
+    },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: "ramping-vus",
+      gracefulStop: "30s",
+      stages: [
+        { target: 5, duration: "30s" },
+        { target: 15, duration: "1m" },
+        { target: 10, duration: "30s" },
+        { target: 0, duration: "30s" },
+      ],
+      gracefulRampDown: "30s",
+      exec: "scenario_1",
+    },
+  },
+};
+
+export function scenario_1() {
+  let response;
+
+  const vars = {};
+
+  group("Login and order - https://pizza.tylerhiattdev.click/", function () {
+    // Homepage
+    response = http.get("https://pizza.tylerhiattdev.click/", {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "if-modified-since": "Tue, 11 Mar 2025 22:24:52 GMT",
+        "if-none-match": '"ab8a55c0a8b2324cac5b04148e1cd6ed"',
+        priority: "u=0, i",
+        "sec-ch-ua":
+          '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+      },
+    });
+    sleep(7.9);
+
+    // Login
+    response = http.put(
+      "https://pizza-service.tylerhiattdev.click/api/auth",
+      '{"email":"tylernhiatt@gmail.com","password":"test"}',
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+          origin: "https://pizza.tylerhiattdev.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+
+    if (
+      !check(response, {
+        "status equals 200": (response) => response.status.toString() === "200",
+      })
+    ) {
+      console.log(response.body);
+      fail("Login was *not* 200");
+    }
+
+    vars["token1"] = jsonpath.query(response.json(), "$.token")[0];
+
+    response = http.options(
+      "https://pizza-service.tylerhiattdev.click/api/auth",
+      null,
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "access-control-request-headers": "content-type",
+          "access-control-request-method": "PUT",
+          origin: "https://pizza.tylerhiattdev.click",
+          priority: "u=1, i",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+    sleep(2.3);
+
+    // Get Menu
+    response = http.get(
+      "https://pizza-service.tylerhiattdev.click/api/order/menu",
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          authorization: `Bearer ${vars["token1"]}`,
+          "content-type": "application/json",
+          origin: "https://pizza.tylerhiattdev.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+
+    response = http.options(
+      "https://pizza-service.tylerhiattdev.click/api/order/menu",
+      null,
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "access-control-request-headers": "authorization,content-type",
+          "access-control-request-method": "GET",
+          origin: "https://pizza.tylerhiattdev.click",
+          priority: "u=1, i",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+
+    // Get Franchise
+    response = http.get(
+      "https://pizza-service.tylerhiattdev.click/api/franchise",
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          authorization: `Bearer ${vars["token1"]}`,
+          "content-type": "application/json",
+          origin: "https://pizza.tylerhiattdev.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+
+    response = http.options(
+      "https://pizza-service.tylerhiattdev.click/api/franchise",
+      null,
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "access-control-request-headers": "authorization,content-type",
+          "access-control-request-method": "GET",
+          origin: "https://pizza.tylerhiattdev.click",
+          priority: "u=1, i",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+    sleep(8.4);
+
+    // Purchase Pizza
+    response = http.post(
+      "https://pizza-service.tylerhiattdev.click/api/order",
+      '{"items":[{"menuId":1,"description":"Veggie","price":0.0038}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          authorization: `Bearer ${vars["token1"]}`,
+          "content-type": "application/json",
+          origin: "https://pizza.tylerhiattdev.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+
+    if (
+      !check(response, {
+        "status equals 200": (response) => response.status.toString() === "200",
+      })
+    ) {
+      console.log(response.body);
+      fail("Purchase was *not* 200");
+    }
+
+    // Extract pizza JWT from the purchase response
+    vars["pizzaJwt"] = jsonpath.query(response.json(), "$.jwt")[0];
+
+    response = http.options(
+      "https://pizza-service.tylerhiattdev.click/api/order",
+      null,
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "access-control-request-headers": "authorization,content-type",
+          "access-control-request-method": "POST",
+          origin: "https://pizza.tylerhiattdev.click",
+          priority: "u=1, i",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      }
+    );
+    sleep(15.1);
+
+    // Verify Pizza
+    response = http.post(
+      "https://pizza-factory.cs329.click/api/order/verify",
+      JSON.stringify({ jwt: vars["pizzaJwt"] }),
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          authorization: `Bearer ${vars["token1"]}`,
+          "content-type": "application/json",
+          origin: "https://pizza.tylerhiattdev.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "cross-site",
+          "sec-fetch-storage-access": "active",
+        },
+      }
+    );
+
+    response = http.options(
+      "https://pizza-factory.cs329.click/api/order/verify",
+      null,
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "access-control-request-headers": "authorization,content-type",
+          "access-control-request-method": "POST",
+          origin: "https://pizza.tylerhiattdev.click",
+          priority: "u=1, i",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "cross-site",
+        },
+      }
+    );
+  });
+}
